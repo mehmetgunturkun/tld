380cb35c2550fe245199f21460c35b9c7f0fe84a
mehmetgunturkun <mehmetcemgunturkun@gmail.com>
2016-03-26 14:37:56 +0200

    NNClassifier update logic is completed

--- src/detector/nn/NearestNeighborClassifier.cc
+++ src/detector/nn/NearestNeighborClassifier.cc
@@ -7,7 +7,6 @@ NearestNeighborClassifier::NearestNeighborClassifier() {
 
 void NearestNeighborClassifier::init(TrainingSet<Box> ts) {
     Frame* frame = ts.frame;
-
     vector<Box*> positiveSamples = ts.positiveSamples;
     for (int i = 0; i < ts.nrOfPositiveSamples; i++) {
         Box* box = positiveSamples[i];
@@ -33,7 +32,27 @@ bool NearestNeighborClassifier::classify(Frame* frame, ScoredBox* scoredBox) {
 }
 
 void NearestNeighborClassifier::update(TrainingSet<ScoredBox> ts) {
+    vector<ScoredBox*> positiveSamples = ts.positiveSamples;
+    for (int i = 0; i < ts.nrOfPositiveSamples; i++) {
+        ScoredBox* scoredBox = positiveSamples[i];
+        NNClassificationDetails* details = (NNClassificationDetails*) scoredBox->getDetail("nn");
+        Patch* patch = details->patch;
+        double score = model->computeRelativeScore(patch);
+        if (score < 0.65) {
+            model->add(patch, true);
+        }
+    }
 
+    vector<ScoredBox*> negativeSamples = ts.negativeSamples;
+    for (int i = 0; i < ts.nrOfNegativeSamples; i++) {
+        ScoredBox* scoredBox = negativeSamples[i];
+        NNClassificationDetails* details = (NNClassificationDetails*) scoredBox->getDetail("nn");
+        Patch* patch = details->patch;
+        double score = model->computeRelativeScore(patch);
+        if (score > 0.5) {
+            model->add(patch, false);
+        }
+    }
 }
 
 double NearestNeighborClassifier::score(Frame* frame, Box* box) {
