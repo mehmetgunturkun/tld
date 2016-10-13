1a836d9df15f4473795378cb2f7614e2209f4312
mehmetgunturkun <mehmetcemgunturkun@gmail.com>
2016-10-13 15:27:21 +0300

    Removed unnec includes and modified color codes

--- multitld/include/common/Colors.hpp
+++ multitld/include/common/Colors.hpp
@@ -3,16 +3,12 @@
 
 #include "opencv2/imgproc/imgproc.hpp"
 
-// #define ANSI_COLOR_GREEN   "\x1b[32m"
-// #define ANSI_COLOR_RED     "\x1b[31m"
-// #define ANSI_COLOR_YELLOW  "\x1b[33m"
-
 #define COLOR_BOLD    "\x1b[1m"
 
-#define COLOR_GREEN   "\x1b[32m"
-#define COLOR_RED     "\x1b[31m"
-#define COLOR_YELLOW  "\x1b[33m"
-#define COLOR_CYAN    "\x1b[36m"
+#define COLOR_RED     "\x1b[91m"
+#define COLOR_GREEN   "\x1b[92m"
+#define COLOR_YELLOW  "\x1b[93m"
+#define COLOR_CYAN    "\x1b[96m"
 #define COLOR_WHITE   "\x1b[37m"
 
 #define COLOR_RESET   "\x1b[0m"
