6628254937ddb830d4fdc3ffe61fa2dd762eaec6
mehmetcemgunturkun <mehmetcemgunturkun@gmail.com>
2017-04-03 21:54:27 +0300

    Added clustering logic to v2

---
+++ v2/tld/include/common/clustering/Distance.hpp
@@ -0,0 +1,12 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+
+class Distance {
+public:
+    int item0;
+    int item1;
+    double value;
+
+    Distance(int i0, int i1, double value);
+};
+#endif
---
+++ v2/tld/src/common/clustering/Distance.cc
@@ -0,0 +1,7 @@
+#include "common/clustering/Distance.hpp"
+
+Distance::Distance(int item0, int item1, double value) {
+    this->item0 = item0;
+    this->item1 = item1;
+    this->value = value;
+}
